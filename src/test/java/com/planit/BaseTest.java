0a1d487fc56680160d404fcb3269833e55f374f9
Matt Donnelly <mail@mattdonnelly.com.au>
2025-03-26 00:26:42 +1100

    Adds common setup and teardowns to BaseTest

--- src/test/java/com/planit/BaseTest.java
+++ src/test/java/com/planit/BaseTest.java
@@ -1,4 +1,30 @@
+// Abstract test class, provides common setup and teardowns for all tests.
+// Centralises driver initialisation using WebDriverFactory
+
 package com.planit;
 
+import com.planit.utils.WebDriverFactory;
+import com.planit.utils.ConfigReader;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
 public class BaseTest {
+    protected WebDriver driver;
+    protected static final String BASE_URL = ConfigReader.getProperty("base.url");
+
+    @BeforeClass
+    public void setUp() {
+        driver = WebDriverFactory.createDriver();
+        driver.get(BASE_URL);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
