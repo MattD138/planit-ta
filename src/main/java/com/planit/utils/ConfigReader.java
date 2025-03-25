2c228a7c9b44f6029d590a27d477759a8a8566be
Matt Donnelly <mail@mattdonnelly.com.au>
2025-03-25 20:20:17 +1100

    Adds dependencies for selenium, testng and webdrivermanager. Implements basic helper classes.

--- src/main/java/com/planit/utils/ConfigReader.java
+++ src/main/java/com/planit/utils/ConfigReader.java
@@ -1,4 +1,25 @@
+// Helper utility for retrieving config properties from config.properties
+
 package com.planit.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+    private static Properties properties = new Properties();
+
+    static {
+        try (FileInputStream file = new FileInputStream("src/test/resources/config.properties")) {
+            properties.load(file);
+        } catch (IOException e) {
+            // Todo: can be improved with better logging
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load configuration file");
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
--- src/main/java/com/planit/utils/WebDriverFactory.java
+++ src/main/java/com/planit/utils/WebDriverFactory.java
@@ -1,4 +1,17 @@
+// Utility for creation and config of WebDriver instances
+// Uses WebDriverManager to manage browser drivers dynamically
+
 package com.planit.utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class WebDriverFactory {
+    public static WebDriver createDriver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
 }
