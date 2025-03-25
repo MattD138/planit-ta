2bf41cd4a18bd76a98bd00ec171f96bb21ba836d
Matt Donnelly <mail@mattdonnelly.com.au>
2025-03-25 22:54:35 +1100

    Adds interactions for ShopPage

--- src/main/java/com/planit/pages/ShopPage.java
+++ src/main/java/com/planit/pages/ShopPage.java
@@ -1,4 +1,36 @@
+// Page object representing the Shop page of the website, located at /shop
+
 package com.planit.pages;
 
-public class ShopPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ShopPage extends BasePage {
+    public ShopPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Add an item to cart by clicking the 'Buy' button multiple times
+    public void addItemToCart(String itemName, int quantity) {
+        By itemContainer = By.xpath("//h4[contains(text(), '" + itemName + "')]/ancestor::div[contains(@class, 'product')]");
+        WebElement container = driver.findElement(itemContainer);
+        WebElement buyButton = container.findElement(By.xpath(".//a[text()='Buy']"));
+        for (int i = 0; i < quantity; i++) {
+            clickElement(buyButton);
+        }
+    }
+
+    // Extract the price displayed for the given item
+    public double getPriceForItem(String itemName) {
+        By priceLocator = By.xpath("//h4[contains(text(), '" + itemName + "')]/following-sibling::p[@class='price']");
+        String priceText = driver.findElement(priceLocator).getText();
+        priceText = priceText.replaceAll("[^\\.\\d]", ""); // Remove non-numeric characters except decimal
+        return Double.parseDouble(priceText);
+    }
+
+    // Navigate to the Cart page via the navbar
+    public void navigateToCart() {
+        clickElement(driver.findElement(By.linkText("Cart")));
+    }
 }
