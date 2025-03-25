29ff0f0a7cfee3fceba2ddf894de3f12d100f39b
Matt Donnelly <mail@mattdonnelly.com.au>
2025-03-25 20:40:56 +1100

    Adds core common actions to BasePage.java

--- src/main/java/com/planit/pages/BasePage.java
+++ src/main/java/com/planit/pages/BasePage.java
@@ -1,4 +1,38 @@
+// Baseline class for all page objects.
+// Implements common core actions (waiting for elements, clicking, entering text)
+
 package com.planit.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void waitForElementToBeVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected void clickElement(WebElement element) {
+        waitForElementToBeVisible(element);
+        element.click();
+    }
+
+    protected void enterText(WebElement element, String text) {
+        waitForElementToBeVisible(element);
+        element.clear();
+        element.sendKeys(text);
+    }
 }
