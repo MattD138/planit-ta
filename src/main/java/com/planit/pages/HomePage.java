216171d76a16c81021d3613ae5c66824f63380e6
Matt Donnelly <mail@mattdonnelly.com.au>
2025-03-25 22:29:31 +1100

    Adds basic page object for HomePage

--- src/main/java/com/planit/pages/HomePage.java
+++ src/main/java/com/planit/pages/HomePage.java
@@ -1,4 +1,23 @@
+// Page object representing the Home page of the website
+
 package com.planit.pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+    private By contactPageLink = By.linkText("Contact");
+    private By shopPageLink = By.linkText("Shop");
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateToContactPage() {
+        clickElement(driver.findElement(contactPageLink));
+    }
+
+    public void navigateToShopPage() {
+        clickElement(driver.findElement(shopPageLink));
+    }
 }
