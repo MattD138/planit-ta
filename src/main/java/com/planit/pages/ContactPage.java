2735aae616a03fca65e6b7286dde067059ee6ee4
Matt Donnelly <mail@mattdonnelly.com.au>
2025-03-25 20:56:24 +1100

    Adds clickSubmitButton function to ContactPage

--- src/main/java/com/planit/pages/ContactPage.java
+++ src/main/java/com/planit/pages/ContactPage.java
@@ -1,4 +1,19 @@
+//Page object representing the Contact page of the website, located at /contact
+
 package com.planit.pages;
 
-public class ContactPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ContactPage extends BasePage {
+    // Locators
+    private By submitButton = By.xpath("//a[text()='Submit']");
+
+    public ContactPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickSubmitButton() {
+        clickElement(driver.findElement(submitButton));
+    }
 }
